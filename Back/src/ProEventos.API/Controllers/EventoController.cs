7187b2b7aeb4247a317caa76fd98b214b1e99588
AngeloTQueiroz <angelotqueiroz@gmail.com>
2026-08-15 21:34:27 -0300

    Api composta pelo SQLite

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 namespace ProEventos.API.Controllers
 {
@@ -12,43 +13,26 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> _evento = new Evento[]
-            {
-                new Evento()  {
-                Id = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "SP",
-                DataEvento = DateTime.Now.ToString("dd/MM/yyyy"),
-                QtdPessoas = 250,
-                Lote = "Lote 1",
-                ImagemURL = "https://example.com/image.jpg"
-                },
-                new Evento()  {
-                Id = 2,
-                Tema = "2",
-                Local = "SP a",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                QtdPessoas = 350,
-                Lote = "Lote 1",
-                ImagemURL = "https://example.com/image.jpg"
-                }
-            };
+ 
 
-        public EventoController()
+        public readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 // rota que retorna todos os eventos
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
 // rota que filtra por ID
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetById(int id)
         {
-            return _evento.Where(evento => evento.Id == id);
+            return _context.Eventos.Where(evento => evento.Id == id);
         }
 
         [HttpPost]
